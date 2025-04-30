cdfeda00026ce7dfa72899365119b8a650c8843a
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2025-04-30 20:40:42 +0500

    Fixed eslint bugs with deplyoment

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -4,11 +4,11 @@ import { CustomerField, InvoiceForm } from '@/app/lib/definitions'
 import { CheckIcon, ClockIcon, CurrencyDollarIcon, UserCircleIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
 import { Button } from '@/app/ui/button'
-import { updateInvoice } from '@/app/lib/actions'
+import { State, updateInvoice } from '@/app/lib/actions'
 import { useActionState } from 'react'
 
 export default function EditInvoiceForm({ invoice, customers }: { invoice: InvoiceForm; customers: CustomerField[] }) {
-        const initialState: any = {}
+        const initialState: State = {}
         const updateInvoiceWithId = updateInvoice.bind(null, invoice.id)
         const [state, formAction] = useActionState(updateInvoiceWithId, initialState)
         return (
